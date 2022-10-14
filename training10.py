51f4448b41b1fbc200968b84248a14106aa5d250
Dalcommagic <ksoo6805@naver.com>
2022-10-14 18:07:51 +0900

    training10

--- training10.py
+++ training10.py
@@ -1,4 +1,4 @@
-import numpy as np, cv2
+#import numpy as np, cv2
 
 '''x = np.array([1,2,3,5,10],np.float32)
 y = np.array([2,5,7,2,9]).astype("float32")
@@ -18,7 +18,7 @@ print("[p_ang]=%s" % np.ravel(p_ang))
 print("[x_mat2]=%s" % x2.flatten())
 print("[y_mat2]=%s" % y2.flatton())'''
 
-def mat_access1(mat):
+'''def mat_access1(mat):
     for i in range(mat.shape[0]):
         for j in range(mat.shape[1]):
             k = mat[i,j]
@@ -39,6 +39,25 @@ print("원소 처리 후 : \n%s\n" % mat1)
 
 print("원소 처리 전 : \n%s\n" % mat2)
 mat_access2(mat2)
-print("원소 처리 후 : \n%s\n" % mat2)
+print("원소 처리 후 : \n%s\n" % mat2)'''
+
+import numpy as np
+import cv2
+
+image=np.zeros((200,400),np.uint8)
+image[:]=200
+
+title1, title2 = 'Position1','Position2'
+cv2.nameWindow(title1,cv2.WINDOW_AUTOSIZE)
+cv2.nameWindow(title2)
+cv2.moveWindow(title1,150,150)
+cv2.moveWindow(title2,400,50)
+
+cv2.imshow(title1, image)
+cv2.imshow(title2,image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+
 
 
