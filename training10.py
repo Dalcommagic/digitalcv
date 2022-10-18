6fea367bd0d0debad8dfba43c2ff4ea329b5fc43
Dalcommagic <ksoo6805@naver.com>
2022-10-18 13:13:53 +0900

    training10

--- training10.py
+++ training10.py
@@ -162,7 +162,7 @@ cv2.imshow("Line & Rectangle", image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()'''
 
-import cv2
+'''import cv2
 
 capture = cv2.VideoCapture(0)   #0번 카메라 연결
 if capture.isOpened() == False: raise Exception("카메라 연결 안됨")
@@ -195,7 +195,7 @@ while True:
     cv2.imshow("View Frame from camera", frame)
 
 writer.release()
-capture.release()
+capture.release()'''
 '''def put_string(frame, tetxt, pt, value, color=(120,200,90)):    #문자열 출력
     text += str(value)
     shade == (pt[0]+2, pt[1] + 2)
@@ -209,7 +209,37 @@ if capture.isOpensed() == False:       #카메라 연결 예외처리
 
 print("너비 %d" % capture.get(cv2.CAP_PROP_FRAME_WIDTH))'''
 
+import numpy as np, cv2, math
 
+def calc_hsi(bgr):  #한 화소 hsi  계산함수
+    B, G, R = float(bgr[0]), float(bgr[1]), float(bgr[2]) #float형 변환
+    bgr_sum=(R+G+B)
+    #색상계산
+    tmp1 = ((R-G) + (R-B)) * 0.5
+    tmp2 = math.sqrt((R-G)*(R-G)+(R-B)*(G-B))
+    angle = math.acos(tmp1 / tmp2) * (180 / np.pi) if tmp2 else 0 #각도
+
+    H = angel if B <= G else 360 - angle    #색상
+    S = 1.0 -3 * min([R,G,B]) / bgr_sum if bgr_sum else 0 #채도
+    I = bgr_sum / 3 #명도
+    return (H/2, S*255, I)   3원소 튜플로 변환
+
+def bgr2hsi(image): #BGR 컬러 - HSI컬러 변환 함수
+    hsv=[[calc_hsi(pixel) for pixel in row]for row in image] #2차원 배열 순회
+    return cv2.convertScaleAbs(np.array(hsv))
+
+BGR_img = cv2.imread("images/color_space.jpg", cv2.IMREAD_COLOR) #컬러영상 읽기
+if BGR_img is None: raise Exception("영상 읽기 오류")
+
+HSI_img = bgr2hsi(BGR_img)      #BGR-HSI변환
+HSV_img = cv2.cvtColor(BGR_img, cv2.COLOR_BGR2HSV)  #OpenCV함수
+Hue, Saturation, Intensity = cv2.split(HSI_img) #채널 분리
+Hue2, Saturation2, Internsity2 = cv2.split(HSV_img) #채널분리
+
+titles = ['BGR_img', 'Hue', 'Saturation', 'Intensity']
+[cv2.imshow(t, eval(t)) for t in titles]    #User 구현 결과 영상표시
+[cv2.imshow('OpenCV_'+t, eval(t+'2'))for t in titles[1:]]   #OpenCV 결과 영상 표시
+cv2.waitKey(0)
 
 
 
